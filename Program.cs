9344e1a6412c9360d5e4cc8d4e23f05800f06112
Elvan Tektaş <tektaselvan.27@gmail.com>
2022-02-11 14:06:19 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿   int time = DateTime.Now.Hour;
+   if (time >= 6 && time < 11)
+   Console.WriteLine("Günaydınn!");
+  
+
+    else if(time<= 18)
+    
+      Console.WriteLine("İyi Günler");
+    
+    else
+        Console.WriteLine("İyi geceler");
+   
+    string sonuc = time<=18 ? "İyi günler" : "İyi geceler";
+    sonuc = time >= 6 && time < 11  ? "Günaydın" : time <= 18 ? "İyi günler" : "İyi geceler";
+    Console.WriteLine(sonuc);
+    
+
